674c67f07fd46c98ab6fb00cd0052111a84e5de6
leetwelves <69631968+leetwelves@users.noreply.github.com>
2020-08-14 14:32:37 +0800

    update dataloader

--- dataloader.py
+++ dataloader.py
@@ -12,33 +12,15 @@ import json
 
 
 class AliDataset(torch.utils.data.Dataset):
-    def __init__(self, dataset_dir,train=True,test=False):
-        self.test=test
-        path = os.path.join(dataset_dir, 'amap_traffic_annotations_train.json')
-        file = open(path, encoding='utf-8')
-        file = json.loads(file.read())
+    def __init__(self, dataset_dir, data_list):
+        path = os.path.join(dataset_dir, data_list)
         self.labels = []
-        for sample in file['annotations']:
-            id = sample['id']
-            status = sample['status']
-            for frame in sample['frames']:
-                frame_name = os.path.join(dataset_dir,id,frame['frame_name'])
-                self.labels.append([frame_name, status])
-        if self.test:
-             self.labels=sorted(self.labels,key=lambda x: int(x.split('.')[-2].split('/')[-1]))
-        else:
-             self.labels=sorted(self.labels,key=lambda x: int(x.split('.')[-2]))
-        
+        file = open(path,'r')
+        for row in file:
+            row = file.strip().split(' ')
+            self.labels.append(row[0], int(row[1]))
         imgs_num = len(self.labels)
 
-        if self.test:
-            self.labels=self.labels
-        elif train:
-            self.labels=self.labels[:int(0.7*imgs_num)]
-        else :
-            self.labels=self.labels[int(0.7*imgs_num):]
-                
-    
     def __getitem__(self, index):
         img_dir = self.labels[index][0]
         img = cv2.imread(img_dir)
@@ -53,4 +35,29 @@ class AliDataset(torch.utils.data.Dataset):
     def __repr__(self):
         return self.__class__.__name__
 
+def Ali_loader(dataset_dir, batch_size, num_workers, use_gpu):
+    assert os.path.exists(dataset_dir)
 
+    train_list_dir = '/Users/stephan/Desktop/阿里天池/train_list.txt'
+    test_list_dir = '/Users/stephan/Desktop/阿里天池/test_list.txt'
+    train_dataset = AliDataset(dataset_dir, train_list_dir)
+    test_list_dir = AliDataset(dataset_dir, test_list_dir)
+    
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=use_gpu,
+        drop_last=True,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=use_gpu,
+        drop_last=False,
+    )
+
+    return train_loader, test_loader
