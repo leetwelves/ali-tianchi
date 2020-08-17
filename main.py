c429726b3810641e1d56e891720cdbbc16848a66
leetwelves <69631968+leetwelves@users.noreply.github.com>
2020-08-17 14:52:43 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import torch as t
 from torch.autograd import Variable
 import models
-from data.dataloader import Ali_loader
+from data.dataloader import Ali_loader,Alitest_loader
 from torch.utils.data import DataLoader
 from torchnet import meter
 from utils.visualize import Visualizer
@@ -14,17 +14,39 @@ from collections import OrderedDict
 os.environ['CUDA_VISIBLE_DEVICES']='0'
 
 
-@t.no_grad() # pytorch>=0.5
+
 def  main():
     train()
 
-def test(**kwargs):
+@t.no_grad() # pytorch>=0.5
+def test(epoch,**kwargs):
     opt._parse(kwargs)
 
     # configure model
+    model=resnet101(pretrained=False)
+    fc_feat_num = model.fc.in_features
+    model.fc = t.nn.Linear(fc_feat_num, 3)
+    pthfile='./checkpoints/model_state_{}.pth'.format(epoch)
+    model.load_state_dict(t.load(pthfile))
+    model.cuda()
    
-
     # data
+    data_dir='/home/apollo/ali-tianchi/dataset/test_dataset'
+    test_loader=Alitest_loader(dataset_dir=data_dir,batch_size=opt.batch_size,num_workers=opt.num_workers,use_gpu=opt.use_gpu)
+
+    results=[]
+    for ii ,(data,path) in enumerate(test_loader):
+        input=t.autograd.Variable(data,volatile =True)
+        input=input.cuda()
+        score=model(input)
+        #print(score)
+        probability = t.nn.functional.softmax\
+            (score).data.cpu().numpy()
+        probability = probability.argmax(1)
+        batch_results = [(path_,probability_)
+            for path_,probability_ in zip(path,probability)]
+        results += batch_results
+    write_csv(results,'./results/model_test_{}.csv'.format(epoch))
     
 
   
@@ -44,25 +66,29 @@ def train(**kwargs):
     model = resnet101(pretrained=False)
     pthfile = './resnet101.pth'
     model.load_state_dict(t.load(pthfile))
+    # for param in model.parameters():
+    #     param.requires_grad = True
+    # for param in model.fc.parameters():
+    #     param.requires_grad = True
     fc_feat_num = model.fc.in_features
     model.fc = t.nn.Linear(fc_feat_num, 3)
-    model.train()
+    # model.train()
     model.cuda()
 
    
 
     # step2: data
     data_dir='/home/apollo/ali-tianchi/dataset/train_dataset'
-    train_loader,test_loader=Ali_loader(dataset_dir=data_dir,batch_size=8,num_workers=8,use_gpu=True)
+    train_loader,val_loader=Ali_loader(dataset_dir=data_dir,batch_size=opt.batch_size,num_workers=opt.num_workers,use_gpu=opt.use_gpu)
     
     # step3: criterion and optimizer
     criterion = t.nn.CrossEntropyLoss()
     lr = opt.lr
-    optimizer=t.optim.SGD(model.parameters(),
+    optimizer=t.optim.SGD( model.parameters(),
                                                         lr=lr,
                                                         weight_decay=opt.weight_decay)
 
-   # optimizer = model.get_optimizer(lr, opt.weight_decay)
+   
     
         
     # step4: meters
@@ -81,15 +107,17 @@ def train(**kwargs):
                 # train model 
                 #input = data.to(opt.device)
                 #target = label.to(opt.device)
-                input=Variable(data)
-                target=Variable(label)
-               
-                input=input.cuda().requires_grad_()
+                #input=Variable(data)
+                #target=Variable(label)
+                input=data
+                target=label
+
+                input=input.cuda()            
                 target=target.cuda()
                 
                 optimizer.zero_grad()
                 score = model(input)
-                loss = criterion(score,target).requires_grad_()
+                loss = criterion(score,target)
                 loss.backward()
                 optimizer.step()
                 # meters update and visualize
@@ -105,14 +133,14 @@ def train(**kwargs):
                         #   import ipdb
                         #  ipdb.set_trace()
 
-        state = OrderedDict(
-                [('state_dict', model.state_dict()),
-                ('optimizer',optimizer.state_dict()) ,
-                ('epoch',epoch)])
-        t.save(state,'./checkpoints/model_state_{}.pth'.format(epoch))
+        #state = OrderedDict(
+        #        [('state_dict', model.state_dict()),
+         #       ('optimizer',optimizer.state_dict()) ,
+          #      ('epoch',epoch)])
+        t.save(model.state_dict(),'./checkpoints/model_state_{}.pth'.format(epoch))
 
             # validate and visualize
-        val_cm,val_accuracy = val(model,test_loader)
+        val_cm,val_accuracy = val(model,val_loader)
 
         vis.plot('val_accuracy',val_accuracy)
         print('val accurary: {:.4f}'.format(val_accuracy))
@@ -128,6 +156,7 @@ def train(**kwargs):
         
 
         previous_loss = loss_meter.value()[0]
+       # test(epoch=epoch)
 
 @t.no_grad()
 def val(model,dataloader):
@@ -138,12 +167,13 @@ def val(model,dataloader):
     confusion_matrix = meter.ConfusionMeter(3)
     for ii, (val_input, label) in tqdm(enumerate(dataloader)):
         val_input = val_input.to(opt.device)
-        score = model(val_input)
+        with t.no_grad():
+            score = model(val_input)
         confusion_matrix.add(score.detach().squeeze(), label.type(t.LongTensor))
 
     model.train()
     cm_value = confusion_matrix.value()
-    accuracy = 100. * (cm_value[0][0] + cm_value[1][1]) / (cm_value.sum())
+    accuracy = 100. * (cm_value[0][0] + cm_value[1][1]+cm_value[2][2]) / (cm_value.sum())
     return confusion_matrix, accuracy
 
 def help():
@@ -167,5 +197,5 @@ def help():
 if __name__=='__main__':
    # import fire
   #  fire.Fire()
-     main()
-
+    main()
+    # test(epoch=38)
